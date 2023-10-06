3e9bc38074d02f593ad26e62fa72ef29a780a2a1
gabrielniino <gabrielnino@edu.unifil.br>
2023-10-06 15:36:37 -0300

    criação da pagina de tutoriais

--- app.js
+++ app.js
@@ -23,3 +23,56 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+// Modal 
+
+const modalViews = document.querySelectorAll('.modal');
+const modalBtns = document.querySelectorAll('.openModal');
+const modalCloses = document.querySelectorAll('.close');
+const modalPages = document.querySelectorAll('.modal-page');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentModalIndex = 0;
+
+function showModalPage(index) {
+	modalPages.forEach((page, i) => {
+		if (i === index) {
+			page.style.display = 'block';
+		} else {
+			page.style.display = 'none';
+		}
+	});
+}
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		currentModalIndex = 0; // Reset to the first page when opening the modal.
+		showModalPage(currentModalIndex);
+		modalViews[i].classList.add('active-modal');
+	});
+});
+
+modalCloses.forEach((modalClose) => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach((modalView) => {
+			modalView.classList.remove('active-modal');
+		});
+	});
+});
+
+nextButton.addEventListener('click', () => {
+	currentModalIndex++;
+	if (currentModalIndex >= modalPages.length) {
+		currentModalIndex = 0; // Start over if at the end.
+	}
+	showModalPage(currentModalIndex);
+});
+
+prevButton.addEventListener('click', () => {
+	currentModalIndex--;
+	if (currentModalIndex < 0) {
+		currentModalIndex = modalPages.length - 1; // Go to the last page if at the beginning.
+	}
+	showModalPage(currentModalIndex);
+});
